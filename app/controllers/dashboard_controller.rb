db71f3e538f4b997ab941bc0a6ea5a7d184bd5ac
Jeremy Peskin <jeremy.p.peskin@gmail.com>
2018-12-29 13:49:27 -0500

    Added total amounts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
   def index
     @expenses = current_user.expenses
     @invoices = current_user.invoices
+    @sum_of_expenses = @expenses.sum(:amount)
+    @sum_of_invoices = @invoices.sum(:amount)
+    @net_income = @sum_of_invoices - @sum_of_expenses
   end
 end
