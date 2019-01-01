49ca8d958ebe731fc3dbdddd689aece37f5bbb3c
Jeremy Peskin <jeremy.p.peskin@gmail.com>
2019-01-01 08:56:51 -0500

    Dashboard only includes paid invoices

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @expenses = current_user.expenses
-    @invoices = current_user.invoices
+    @invoices = current_user.invoices.where(status: 1)
     @sum_of_expenses = @expenses.sum(:amount)
     @sum_of_invoices = @invoices.sum(:amount)
     @net_income = @sum_of_invoices - @sum_of_expenses
